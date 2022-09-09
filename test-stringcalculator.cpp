16d36b525b2be928d67fce4abf57af0b28c25718
nitya-dyuthi <110531551+nitya-dyuthi@users.noreply.github.com>
2022-09-09 15:58:17 +0530

    Update test-stringcalculator.cpp

--- test-stringcalculator.cpp
+++ test-stringcalculator.cpp
@@ -1,1 +1,20 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
+#include "typewise-alert.h"
+#include "stdlib.h"
+
+TEST_CASE("given empty string and zero is expected") {
+  REQUIRE(add("") == 0);
+}
+
+
+//IMPLEMENTATION
+
+int add(string);
+
+int add(string str) {
+  if(strlen(str) == 0)
+    return 0;
+  return 1;
+}
