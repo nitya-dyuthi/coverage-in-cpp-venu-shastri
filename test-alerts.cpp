4b656cbb78ff3ff75a163c4f1d3fcb22a392739f
nitya-dyuthi <110531551+nitya-dyuthi@users.noreply.github.com>
2022-09-12 13:20:11 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,10 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits lower limit") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach according to limits upper limit") {
+  REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
 }
