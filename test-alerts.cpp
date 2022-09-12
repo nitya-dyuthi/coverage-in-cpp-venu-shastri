a3c87388650e9351742b0946e8e71f9a196ee259
nitya-dyuthi <110531551+nitya-dyuthi@users.noreply.github.com>
2022-09-12 13:54:43 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,3 +10,19 @@ TEST_CASE("infers the breach according to limits lower limit") {
 TEST_CASE("infers the breach according to limits upper limit") {
   REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
 }
+
+TEST_CASE("infers the breach according to limits normal") {
+  REQUIRE(inferBreach(25, 20, 30) == TOO_HIGH);
+}
+
+TEST_CASE("check init upper limit for passive cooling") {
+  REQUIRE(initUpperLimit(PASSIVE_COOLING) == 35);
+}
+
+TEST_CASE("check init upper limit for high active cooling") {
+  REQUIRE(initUpperLimit(HI_ACTIVE_COOLING) == 45);
+}
+
+TEST_CASE("check init upper limit for medium active cooling") {
+  REQUIRE(initUpperLimit(MED_ACTIVE_COOLING) == 40);
+}
